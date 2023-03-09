5a54bc80cded516f6192fee175434e2c7abda6c9
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-03-08 22:16:00 -0700

    Start to build functions for responsive navigation and listeners for window resize

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('hi')
+let windowHeight, windowWidth
+
+const navCheckBox = document.getElementById('navToggle')
+
+window.addEventListener('resize', () => {
+  windowHeight = window.innerHeight
+  windowWidth = window.innerWidth
+})
+
+navCheckBox.addEventListener('onclick', () => {
+  console.log('hi')
+})
