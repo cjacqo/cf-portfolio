fbaea60267222948756d367d45da123639c85747
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-03-09 06:39:12 -0700

    Revert "Start to build functions for responsive navigation and listeners for window resize"
    This reverts commit 5a54bc80cded516f6192fee175434e2c7abda6c9.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,1 @@
-let windowHeight, windowWidth
-
-const navCheckBox = document.getElementById('navToggle')
-
-window.addEventListener('resize', () => {
-  windowHeight = window.innerHeight
-  windowWidth = window.innerWidth
-})
-
-navCheckBox.addEventListener('onclick', () => {
-  console.log('hi')
-})
+console.log('hi')
