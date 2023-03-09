555e787439722340c7393b7e4fe1559769662331
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-03-09 06:38:49 -0700

    Revert "Added responsive class names to navigation"
    This reverts commit a8d106763dd602367e0ef0407d08d960c028b231.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ let windowWidth = window.innerWidth
 let windowHeight = window.innerHeight
 
 const navCheckBox = document.getElementById('navToggle')
-const navList = document.getElementById('navigationList')
 
 window.addEventListener('load', () => {
   if (navCheckBox.getAttribute('checked') !== false) navCheckBox.checked = false
@@ -20,14 +19,8 @@ navCheckBox.addEventListener('change', function() {
 
 function handleNavigation() {
   if (windowWidth >= 750) return
-  console.log(navCheckBox.checked)
 
-  if (navCheckBox.checked) {
-    console.log('hi')
-    navList.classList.remove('hidden')
-    navList.classList.add('visible')
-  } else {
-    navList.classList.remove('visible')
-    navList.classList.add('hidden')
+  if (navCheckBox.getAttribute('checked') === true) {
+    
   }
 }
